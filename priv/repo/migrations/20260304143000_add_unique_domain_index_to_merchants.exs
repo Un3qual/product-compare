2149c71540487c211634f62d16ef43df4069fc0f
Ryan <Un3qual@users.noreply.github.com>
2026-03-04 14:18:53 -0800

    Delete priv/repo/migrations/20260304143000_add_unique_domain_index_to_merchants.exs

--- priv/repo/migrations/20260304143000_add_unique_domain_index_to_merchants.exs
+++
@@ -1,7 +0,0 @@
-defmodule ProductCompare.Repo.Migrations.AddUniqueDomainIndexToMerchants do
-  use Ecto.Migration
-
-  def change do
-    create unique_index(:merchants, [:domain], where: "domain IS NOT NULL")
-  end
-end
