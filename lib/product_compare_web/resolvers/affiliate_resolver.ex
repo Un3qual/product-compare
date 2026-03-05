4095efd6b7e8817270e3f6f1862fc6f18aa0bc24
Ryan <Un3qual@users.noreply.github.com>
2026-03-04 16:43:19 -0800

    affiliate: expose affiliate workflows via graphql

---
+++ lib/product_compare_web/resolvers/affiliate_resolver.ex
@@ -0,0 +1,120 @@
+defmodule ProductCompareWeb.Resolvers.AffiliateResolver do
+  @moduledoc false
+
+  alias ProductCompare.Affiliate
+
+  @spec upsert_affiliate_network(any(), map(), Absinthe.Resolution.t()) ::
+          {:ok, map()} | {:error, String.t()}
+  def upsert_affiliate_network(_parent, %{input: input}, %{
+        context: %{current_user: _current_user}
+      }) do
+    case Affiliate.upsert_network(Map.take(input, [:name])) do
+      {:ok, network} ->
+        {:ok, %{network: network}}
+
+      {:error, changeset} ->
+        {:error, first_changeset_error(changeset)}
+    end
+  end
+
+  def upsert_affiliate_network(_parent, _args, _resolution), do: {:error, "unauthorized"}
+
+  @spec upsert_affiliate_program(any(), map(), Absinthe.Resolution.t()) ::
+          {:ok, map()} | {:error, String.t()}
+  def upsert_affiliate_program(_parent, %{input: input}, %{
+        context: %{current_user: _current_user}
+      }) do
+    with {:ok, attrs} <- normalize_ids(input, [:affiliate_network_id, :merchant_id]),
+         {:ok, program} <- Affiliate.upsert_program(attrs) do
+      {:ok, %{program: program}}
+    else
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:error, first_changeset_error(changeset)}
+
+      {:error, reason} when is_binary(reason) ->
+        {:error, reason}
+    end
+  end
+
+  def upsert_affiliate_program(_parent, _args, _resolution), do: {:error, "unauthorized"}
+
+  @spec upsert_affiliate_link(any(), map(), Absinthe.Resolution.t()) ::
+          {:ok, map()} | {:error, String.t()}
+  def upsert_affiliate_link(_parent, %{input: input}, %{context: %{current_user: _current_user}}) do
+    with {:ok, attrs} <- normalize_ids(input, [:merchant_product_id, :affiliate_network_id]),
+         {:ok, link} <- Affiliate.upsert_link(attrs) do
+      {:ok, %{link: link}}
+    else
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:error, first_changeset_error(changeset)}
+
+      {:error, reason} when is_binary(reason) ->
+        {:error, reason}
+    end
+  end
+
+  def upsert_affiliate_link(_parent, _args, _resolution), do: {:error, "unauthorized"}
+
+  @spec create_coupon(any(), map(), Absinthe.Resolution.t()) ::
+          {:ok, map()} | {:error, String.t()}
+  def create_coupon(_parent, %{input: input}, %{context: %{current_user: _current_user}}) do
+    with {:ok, attrs} <- normalize_ids(input, [:merchant_id, :affiliate_network_id, :artifact_id]),
+         {:ok, coupon} <- Affiliate.create_coupon(attrs) do
+      {:ok, %{coupon: coupon}}
+    else
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:error, first_changeset_error(changeset)}
+
+      {:error, reason} when is_binary(reason) ->
+        {:error, reason}
+    end
+  end
+
+  def create_coupon(_parent, _args, _resolution), do: {:error, "unauthorized"}
+
+  @spec active_coupons(any(), map(), Absinthe.Resolution.t()) ::
+          {:ok, map()} | {:error, String.t()}
+  def active_coupons(_parent, %{input: input}, %{context: %{current_user: _current_user}}) do
+    with {:ok, %{merchant_id: merchant_id} = attrs} <- normalize_ids(input, [:merchant_id]) do
+      now = Map.get(attrs, :at, DateTime.utc_now())
+      {:ok, %{coupons: Affiliate.list_active_coupons(merchant_id, now)}}
+    end
+  end
+
+  def active_coupons(_parent, _args, _resolution), do: {:error, "unauthorized"}
+
+  defp normalize_ids(attrs, id_fields) when is_map(attrs) do
+    Enum.reduce_while(id_fields, {:ok, attrs}, fn field, {:ok, acc} ->
+      case Map.fetch(acc, field) do
+        {:ok, value} ->
+          case cast_id(value) do
+            {:ok, cast_value} ->
+              {:cont, {:ok, Map.put(acc, field, cast_value)}}
+
+            :error ->
+              {:halt, {:error, "invalid #{field}"}}
+          end
+
+        :error ->
+          {:cont, {:ok, acc}}
+      end
+    end)
+  end
+
+  defp cast_id(nil), do: {:ok, nil}
+  defp cast_id(value) when is_integer(value) and value > 0, do: {:ok, value}
+
+  defp cast_id(value) when is_binary(value) do
+    case Integer.parse(value) do
+      {parsed_value, ""} when parsed_value > 0 -> {:ok, parsed_value}
+      _ -> :error
+    end
+  end
+
+  defp cast_id(_value), do: :error
+
+  defp first_changeset_error(changeset) do
+    {_field, {message, _opts}} = List.first(changeset.errors)
+    message
+  end
+end
--- lib/product_compare_web/schema.ex
+++ lib/product_compare_web/schema.ex
@@ -4,6 +4,7 @@ defmodule ProductCompareWeb.Schema do
   import_types(Absinthe.Type.Custom)
 
   alias ProductCompareWeb.GraphQL.GlobalId
+  alias ProductCompareWeb.Resolvers.AffiliateResolver
   alias ProductCompareWeb.Resolvers.AuthResolver
 
   query do
@@ -19,6 +20,13 @@ defmodule ProductCompareWeb.Schema do
       arg(:status, :api_token_status_filter)
 
       resolve(&AuthResolver.my_api_tokens/3)
+    end
+
+    @desc "Returns active coupons for a merchant at a specific timestamp (or now by default)."
+    field :active_coupons, :active_coupons_payload do
+      arg(:input, non_null(:active_coupons_input))
+
+      resolve(&AffiliateResolver.active_coupons/3)
     end
   end
 
@@ -46,6 +54,144 @@ defmodule ProductCompareWeb.Schema do
 
       resolve(&AuthResolver.rotate_api_token/3)
     end
+
+    @desc "Upserts an affiliate network by name."
+    field :upsert_affiliate_network, :upsert_affiliate_network_payload do
+      arg(:input, non_null(:upsert_affiliate_network_input))
+
+      resolve(&AffiliateResolver.upsert_affiliate_network/3)
+    end
+
+    @desc "Upserts an affiliate program by affiliate network and merchant."
+    field :upsert_affiliate_program, :upsert_affiliate_program_payload do
+      arg(:input, non_null(:upsert_affiliate_program_input))
+
+      resolve(&AffiliateResolver.upsert_affiliate_program/3)
+    end
+
+    @desc "Upserts an affiliate link by merchant product."
+    field :upsert_affiliate_link, :upsert_affiliate_link_payload do
+      arg(:input, non_null(:upsert_affiliate_link_input))
+
+      resolve(&AffiliateResolver.upsert_affiliate_link/3)
+    end
+
+    @desc "Creates a coupon for a merchant."
+    field :create_coupon, :create_coupon_payload do
+      arg(:input, non_null(:create_coupon_input))
+
+      resolve(&AffiliateResolver.create_coupon/3)
+    end
+  end
+
+  input_object :upsert_affiliate_network_input do
+    field :name, non_null(:string)
+  end
+
+  input_object :upsert_affiliate_program_input do
+    field :affiliate_network_id, non_null(:id)
+    field :merchant_id, non_null(:id)
+    field :program_code, :string
+    field :status, :string
+  end
+
+  input_object :upsert_affiliate_link_input do
+    field :merchant_product_id, non_null(:id)
+    field :affiliate_network_id, :id
+    field :original_url, non_null(:string)
+    field :affiliate_url, non_null(:string)
+    field :last_verified_at, :datetime
+  end
+
+  input_object :create_coupon_input do
+    field :merchant_id, non_null(:id)
+    field :affiliate_network_id, :id
+    field :artifact_id, :id
+    field :code, non_null(:string)
+    field :description, :string
+    field :discount_type, non_null(:coupon_discount_type)
+    field :discount_value, :decimal
+    field :currency, :string
+    field :valid_from, :datetime
+    field :valid_to, :datetime
+    field :terms, :string
+  end
+
+  input_object :active_coupons_input do
+    field :merchant_id, non_null(:id)
+    field :at, :datetime
+  end
+
+  object :upsert_affiliate_network_payload do
+    field :network, non_null(:affiliate_network)
+  end
+
+  object :upsert_affiliate_program_payload do
+    field :program, non_null(:affiliate_program)
+  end
+
+  object :upsert_affiliate_link_payload do
+    field :link, non_null(:affiliate_link)
+  end
+
+  object :create_coupon_payload do
+    field :coupon, non_null(:coupon)
+  end
+
+  object :active_coupons_payload do
+    field :coupons, non_null(list_of(non_null(:coupon)))
+  end
+
+  object :affiliate_network do
+    field :id, non_null(:id)
+    field :name, non_null(:string)
+    field :inserted_at, non_null(:datetime)
+    field :updated_at, non_null(:datetime)
+  end
+
+  object :affiliate_program do
+    field :id, non_null(:id)
+    field :affiliate_network_id, non_null(:id)
+    field :merchant_id, non_null(:id)
+    field :program_code, :string
+    field :status, :string
+    field :inserted_at, non_null(:datetime)
+    field :updated_at, non_null(:datetime)
+  end
+
+  object :affiliate_link do
+    field :id, non_null(:id)
+    field :merchant_product_id, non_null(:id)
+    field :affiliate_network_id, :id
+    field :original_url, non_null(:string)
+    field :affiliate_url, non_null(:string)
+    field :last_verified_at, :datetime
+    field :inserted_at, non_null(:datetime)
+    field :updated_at, non_null(:datetime)
+  end
+
+  object :coupon do
+    field :id, non_null(:id)
+    field :merchant_id, non_null(:id)
+    field :affiliate_network_id, :id
+    field :artifact_id, :id
+    field :code, non_null(:string)
+    field :description, :string
+    field :discount_type, non_null(:coupon_discount_type)
+    field :discount_value, :decimal
+    field :currency, :string
+    field :valid_from, :datetime
+    field :valid_to, :datetime
+    field :terms, :string
+    field :inserted_at, non_null(:datetime)
+    field :updated_at, non_null(:datetime)
+  end
+
+  enum :coupon_discount_type do
+    value(:percent)
+    value(:amount)
+    value(:free_shipping)
+    value(:other)
   end
 
   object :create_api_token_payload do
