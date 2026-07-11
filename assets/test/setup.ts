c1501a3d16469682f0725d78fee3369fc2805659
Ryan <Un3qual@users.noreply.github.com>
2026-07-11 14:15:57 -0700

    test: make ResizeObserver stub stateful

--- assets/test/setup.ts
+++ assets/test/setup.ts
@@ -1,9 +1,19 @@
 import "@testing-library/jest-dom/vitest";
 
 class TestResizeObserver implements ResizeObserver {
-  disconnect = () => undefined;
-  observe = () => undefined;
-  unobserve = () => undefined;
+  private readonly observedElements = new Set<Element>();
+
+  disconnect() {
+    this.observedElements.clear();
+  }
+
+  observe(target: Element) {
+    this.observedElements.add(target);
+  }
+
+  unobserve(target: Element) {
+    this.observedElements.delete(target);
+  }
 }
 
 globalThis.ResizeObserver ??= TestResizeObserver;
