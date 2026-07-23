9730fee473e33b22fd5e3a291dff171c1444d842
Ryan <Un3qual@users.noreply.github.com>
2026-07-22 18:11:27 -0700

    refactor: isolate ingestion run ownership

--- lib/product_compare/ingestion.ex
+++ lib/product_compare/ingestion.ex
@@ -12,6 +12,7 @@ defmodule ProductCompare.Ingestion do
   alias ProductCompare.Catalog.GTIN
   alias ProductCompare.Ingestion.NormalizedListing
   alias ProductCompare.Ingestion.Reconciliation
+  alias ProductCompare.Ingestion.Runs
   alias ProductCompare.Pricing
   alias ProductCompare.Repo
   alias ProductCompare.Specs
@@ -51,60 +52,12 @@ defmodule ProductCompare.Ingestion do
   ]
 
   @spec start_import_run(map()) :: {:ok, ImportRun.t()} | {:error, Ecto.Changeset.t()}
-  def start_import_run(attrs) do
-    attrs =
-      attrs
-      |> Map.new()
-      |> prepare_reconciliation()
-      |> Map.put_new(:status, "running")
-      |> Map.put_new(:started_at, DateTime.utc_now())
-
-    %ImportRun{}
-    |> ImportRun.changeset(attrs)
-    |> Repo.insert()
-  end
+  def start_import_run(attrs), do: Runs.start_import_run(attrs)
 
   @spec complete_import_run(ImportRun.t(), map()) ::
           {:ok, ImportRun.t()} | {:error, Ecto.Changeset.t()}
-  def complete_import_run(%ImportRun{} = import_run, attrs) do
-    attrs =
-      attrs
-      |> Map.new()
-      |> Map.put_new(:finished_at, DateTime.utc_now())
-
-    Repo.transaction(fn ->
-      with {:ok, completed_run} <-
-             import_run
-             |> ImportRun.changeset(attrs)
-             |> Repo.update(),
-           {:ok, reconciled_run} <- Reconciliation.finalize(completed_run) do
-        reconciled_run
-      else
-        {:error, reason} -> Repo.rollback(reason)
-      end
-    end)
-  end
-
-  defp prepare_reconciliation(attrs) do
-    {complete_scope, attrs} = Map.pop(attrs, :complete_scope, false)
-
-    attrs
-    |> normalize_complete_scope_cursor(complete_scope)
-    |> Map.put_new(:scope_fingerprint, Reconciliation.scope_fingerprint(attrs))
-    |> Map.put_new(
-      :reconciliation_status,
-      if(complete_scope == true, do: "pending", else: "not_requested")
-    )
-  end
-
-  defp normalize_complete_scope_cursor(attrs, true) do
-    Map.update(attrs, :cursor_start, 0, fn
-      nil -> 0
-      cursor -> cursor
-    end)
-  end
-
-  defp normalize_complete_scope_cursor(attrs, _complete_scope), do: attrs
+  def complete_import_run(%ImportRun{} = import_run, attrs),
+    do: Runs.complete_import_run(import_run, attrs)
 
   @spec upsert_merchant_feed_candidate(Source.t(), map()) ::
           {:ok, MerchantFeedCandidate.t()} | {:error, Ecto.Changeset.t()}
