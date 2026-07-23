3c8180ca9a1f0e69da066a0d7aa4b2c1e90c9d2b
Ryan <Un3qual@users.noreply.github.com>
2026-07-22 20:11:47 -0700

    refactor: isolate alert inbox ownership

--- lib/product_compare/alerts.ex
+++ lib/product_compare/alerts.ex
@@ -6,6 +6,7 @@ defmodule ProductCompare.Alerts do
   import Ecto.Query
 
   alias ProductCompare.Alerts.MarketFacts
+  alias ProductCompare.Alerts.Inbox
   alias ProductCompare.Alerts.WatchRules
   alias ProductCompare.Repo
   alias ProductCompareSchemas.Alerts.AlertDeliveryAttempt
@@ -31,11 +32,7 @@ defmodule ProductCompare.Alerts do
 
   @spec list_alert_events_query(pos_integer(), keyword()) :: Ecto.Query.t()
   def list_alert_events_query(user_id, opts \\ []) do
-    AlertEvent
-    |> where([event], event.user_id == ^user_id)
-    |> maybe_filter_unread(Keyword.get(opts, :unread_only, false))
-    |> order_by([event], desc: event.inserted_at, desc: event.id)
-    |> preload([:triggering_price_point, :watch_rule, merchant_product: [:merchant, :product]])
+    Inbox.list_alert_events_query(user_id, opts)
   end
 
   @spec update_watch(pos_integer(), Ecto.UUID.t(), map()) ::
@@ -57,19 +54,7 @@ defmodule ProductCompare.Alerts do
   @spec mark_alert_read(pos_integer(), Ecto.UUID.t()) ::
           {:ok, AlertEvent.t()} | {:error, :not_found | Ecto.Changeset.t()}
   def mark_alert_read(user_id, entropy_id) when valid_id(user_id) and is_binary(entropy_id) do
-    case Repo.get_by(AlertEvent, user_id: user_id, entropy_id: entropy_id) do
-      nil ->
-        {:error, :not_found}
-
-      %AlertEvent{read_at: %DateTime{}} = event ->
-        {:ok, load_alert_event(event)}
-
-      event ->
-        event
-        |> AlertEvent.read_changeset(DateTime.utc_now())
-        |> Repo.update()
-        |> load_alert_event()
-    end
+    Inbox.mark_alert_read(user_id, entropy_id)
   end
 
   def mark_alert_read(_user_id, _entropy_id), do: {:error, :not_found}
@@ -338,19 +323,6 @@ defmodule ProductCompare.Alerts do
 
   defp decimal_lte?(_left, _right), do: false
 
-  defp maybe_filter_unread(query, true), do: where(query, [event], is_nil(event.read_at))
-  defp maybe_filter_unread(query, _unread_only), do: query
   defp decimal_string(%Decimal{} = value), do: Decimal.to_string(value, :normal)
   defp decimal_string(_value), do: nil
-
-  defp load_alert_event({:ok, event}), do: {:ok, load_alert_event(event)}
-  defp load_alert_event({:error, _reason} = error), do: error
-
-  defp load_alert_event(event) do
-    Repo.preload(event, [
-      :triggering_price_point,
-      :watch_rule,
-      merchant_product: [:merchant, :product]
-    ])
-  end
 end
