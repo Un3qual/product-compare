27e4728f097387526ddd8762c4d15acd8ca20f68
Ryan <Un3qual@users.noreply.github.com>
2026-07-23 11:57:57 -0700

    refactor: isolate catalog resolver discovery

--- lib/product_compare_web/resolvers/catalog_resolver.ex
+++ lib/product_compare_web/resolvers/catalog_resolver.ex
@@ -4,8 +4,6 @@ defmodule ProductCompareWeb.Resolvers.CatalogResolver do
   import Absinthe.Resolution.Helpers, only: [on_load: 2]
 
   alias ProductCompare.Catalog
-  alias ProductCompare.Catalog.Filtering
-  alias ProductCompare.Repo
   alias ProductCompare.Specs
   alias ProductCompare.Specs.ClaimValue
   alias ProductCompareWeb.GraphQL.AuthorizedConnection
@@ -13,7 +11,7 @@ defmodule ProductCompareWeb.Resolvers.CatalogResolver do
   alias ProductCompareWeb.GraphQL.Errors, as: GraphQLErrors
   alias ProductCompareWeb.GraphQL.GlobalId
   alias ProductCompareWeb.GraphQL.Input
-  alias ProductCompareWeb.GraphQL.Loader
+  alias ProductCompareWeb.Resolvers.Catalog.Discovery
   alias ProductCompareSchemas.Catalog.Product
   alias ProductCompareSchemas.Catalog.SavedComparisonSet
   alias ProductCompareSchemas.Specs.ProductAttributeCurrent
@@ -22,105 +20,52 @@ defmodule ProductCompareWeb.Resolvers.CatalogResolver do
 
   @base_unit_symbol_cache_context_key :catalog_base_unit_symbol_cache_key
   @max_evidence_excerpt_length 500
-  @max_comparison_products 3
-  @max_search_query_length 100
-
   @spec product(any(), map(), Absinthe.Resolution.t()) ::
           {:ok, Product.t() | nil} | Absinthe.Resolution.Helpers.dataloader_tuple()
-  def product(_parent, args, %{context: %{loader: loader}} = resolution) do
+  def product(parent, args, %{context: %{loader: _loader}} = resolution) do
     clear_base_unit_symbol_cache(resolution)
-    slug = Input.fetch_value(args || %{}, :slug)
-    source = Loader.public_slug_source()
-
-    loader
-    |> Dataloader.load(source, :product, slug)
-    |> on_load(fn loader ->
-      {:ok, Dataloader.get(loader, source, :product, slug)}
-    end)
+    Discovery.product(parent, args, resolution)
   end
 
-  def product(_parent, args, resolution) do
+  def product(parent, args, resolution) do
     clear_base_unit_symbol_cache(resolution)
-    {:ok, Catalog.get_product_by_slug(Input.fetch_value(args || %{}, :slug))}
+    Discovery.product(parent, args, resolution)
   end
 
   @spec comparison_products(any(), map(), Absinthe.Resolution.t()) ::
           {:ok, [Product.t() | nil]}
           | {:error, String.t()}
           | Absinthe.Resolution.Helpers.dataloader_tuple()
-  def comparison_products(_parent, args, %{context: %{loader: loader}} = resolution) do
+  def comparison_products(parent, args, %{context: %{loader: _loader}} = resolution) do
     clear_base_unit_symbol_cache(resolution)
-
-    with {:ok, slugs} <- normalize_comparison_slugs(Input.fetch_list_value(args || %{}, :slugs)) do
-      source = Loader.comparison_source()
-
-      loader
-      |> Dataloader.load(source, :products, slugs)
-      |> on_load(fn loader ->
-        {:ok, Dataloader.get(loader, source, :products, slugs)}
-      end)
-    end
+    Discovery.comparison_products(parent, args, resolution)
   end
 
-  def comparison_products(_parent, args, resolution) do
+  def comparison_products(parent, args, resolution) do
     clear_base_unit_symbol_cache(resolution)
-
-    with {:ok, slugs} <- normalize_comparison_slugs(Input.fetch_list_value(args || %{}, :slugs)) do
-      {:ok, Catalog.list_products_by_slugs(slugs)}
-    end
+    Discovery.comparison_products(parent, args, resolution)
   end
 
   @spec products(any(), map(), Absinthe.Resolution.t()) ::
           {:ok, map()} | {:error, String.t()} | Absinthe.Resolution.Helpers.dataloader_tuple()
-  def products(_parent, args, %{context: %{loader: loader}} = resolution) do
+  def products(parent, args, %{context: %{loader: _loader}} = resolution) do
     clear_base_unit_symbol_cache(resolution)
-
-    with {:ok, filters} <- normalize_filters(Input.fetch_value(args || %{}, :filters, %{})),
-         connection_args = Input.connection_args(args),
-         {:ok, _window} <- Connection.batch_window_result(connection_args) do
-      source = Loader.discovery_root_source()
-      batch_key = {:products, filters, connection_args}
-
-      loader
-      |> Dataloader.load(source, batch_key, :root)
-      |> on_load(fn loader ->
-        {:ok, Dataloader.get(loader, source, batch_key, :root)}
-      end)
-    end
+    Discovery.products(parent, args, resolution)
   end
 
-  def products(_parent, args, resolution) do
+  def products(parent, args, resolution) do
     clear_base_unit_symbol_cache(resolution)
-
-    with {:ok, filters} <- normalize_filters(Input.fetch_value(args || %{}, :filters, %{})) do
-      query = Filtering.apply_filters(Product, filters)
-
-      connection_args = Input.connection_args(args)
-
-      Connection.from_query_result(query, connection_args, Repo)
-    end
+    Discovery.products(parent, args, resolution)
   end
 
   @spec product_filter_metadata(any(), map(), Absinthe.Resolution.t()) ::
           {:ok, map()} | {:error, String.t()} | Absinthe.Resolution.Helpers.dataloader_tuple()
-  def product_filter_metadata(_parent, args, %{context: %{loader: loader}}) do
-    with {:ok, filters} <- normalize_filters(Input.fetch_value(args || %{}, :filters, %{})) do
-      source = Loader.discovery_root_source()
-      batch_key = {:product_filter_metadata, filters}
-
-      loader
-      |> Dataloader.load(source, batch_key, :root)
-      |> on_load(fn loader ->
-        {:ok, Dataloader.get(loader, source, batch_key, :root)}
-      end)
-    end
+  def product_filter_metadata(parent, args, %{context: %{loader: _loader}} = resolution) do
+    Discovery.product_filter_metadata(parent, args, resolution)
   end
 
-  def product_filter_metadata(_parent, args, _resolution) do
-    with {:ok, filters} <- normalize_filters(Input.fetch_value(args || %{}, :filters, %{})) do
-      {:ok, Catalog.product_filter_metadata(filters)}
-    end
-  end
+  def product_filter_metadata(parent, args, resolution),
+    do: Discovery.product_filter_metadata(parent, args, resolution)
 
   @spec current_attributes(Product.t(), map(), Absinthe.Resolution.t()) ::
           {:ok, [map()]} | Absinthe.Resolution.Helpers.dataloader_tuple()
@@ -284,247 +229,6 @@ defmodule ProductCompareWeb.Resolvers.CatalogResolver do
   def delete_saved_comparison_set(_parent, _args, _resolution),
     do: {:ok, saved_comparison_error_payload(GraphQLErrors.unauthenticated_mutation_error())}
 
-  @spec normalize_filters(map() | nil) :: {:ok, map()} | {:error, String.t()}
-  defp normalize_comparison_slugs(slugs)
-       when is_list(slugs) and length(slugs) in 1..@max_comparison_products do
-    normalized_slugs =
-      Enum.map(slugs, fn
-        slug when is_binary(slug) -> String.trim(slug)
-        _value -> nil
-      end)
-
-    if Enum.all?(normalized_slugs, &(is_binary(&1) and &1 != "")) and
-         Enum.uniq(normalized_slugs) == normalized_slugs do
-      {:ok, normalized_slugs}
-    else
-      {:error, "comparison slugs must be unique non-blank strings"}
-    end
-  end
-
-  defp normalize_comparison_slugs(_slugs),
-    do: {:error, "comparison slugs must contain between 1 and 3 values"}
-
-  defp normalize_filters(nil), do: {:ok, %{}}
-
-  defp normalize_filters(filters) when is_map(filters) do
-    with {:ok, query} <- normalize_search_query(Input.fetch_value(filters, :query)),
-         {:ok, sort} <- normalize_product_sort(Input.fetch_value(filters, :sort)),
-         {:ok, primary_type_taxon_id} <-
-           Input.decode_optional_integer_id(
-             Input.fetch_value(filters, :primary_type_taxon_id),
-             :taxon,
-             "taxon"
-           ),
-         {:ok, include_type_descendants} <-
-           Input.normalize_boolean_value(
-             Input.fetch_value(filters, :include_type_descendants, false)
-           ),
-         {:ok, numeric_filters} <-
-           normalize_numeric_filters(Input.fetch_list_value(filters, :numeric)),
-         {:ok, boolean_filters} <-
-           normalize_boolean_filters(Input.fetch_list_value(filters, :booleans)),
-         {:ok, enum_filters} <- normalize_enum_filters(Input.fetch_list_value(filters, :enums)),
-         {:ok, use_case_taxon_ids} <-
-           Input.decode_integer_id_list(
-             Input.fetch_list_value(filters, :use_case_taxon_ids),
-             :taxon,
-             "taxon",
-             "invalid filter ids"
-           ),
-         :ok <- validate_filter_semantics(numeric_filters, boolean_filters, enum_filters) do
-      normalized_filters =
-        %{
-          include_type_descendants: include_type_descendants,
-          numeric: numeric_filters,
-          booleans: boolean_filters,
-          enums: enum_filters,
-          use_case_taxon_ids: use_case_taxon_ids
-        }
-        |> Input.put_present(:query, query)
-        |> Input.put_present(:sort, sort)
-        |> Input.put_present(:primary_type_taxon_id, primary_type_taxon_id)
-
-      {:ok, normalized_filters}
-    end
-  end
-
-  defp normalize_filters(_filters), do: {:error, "invalid filters"}
-
-  defp normalize_search_query(nil), do: {:ok, nil}
-
-  defp normalize_search_query(value) when is_binary(value) do
-    query = String.trim(value)
-
-    cond do
-      query == "" -> {:ok, nil}
-      String.length(query) > @max_search_query_length -> {:error, "search query is too long"}
-      true -> {:ok, query}
-    end
-  end
-
-  defp normalize_search_query(_value), do: {:error, "invalid search query"}
-
-  defp normalize_product_sort(nil), do: {:ok, nil}
-
-  defp normalize_product_sort(sort)
-       when sort in [:id_asc, :name_asc, :brand_name_asc, :newest],
-       do: {:ok, sort}
-
-  defp normalize_product_sort(sort) when is_binary(sort) do
-    case sort |> String.trim() |> String.upcase() do
-      "ID_ASC" -> {:ok, :id_asc}
-      "NAME_ASC" -> {:ok, :name_asc}
-      "BRAND_NAME_ASC" -> {:ok, :brand_name_asc}
-      "NEWEST" -> {:ok, :newest}
-      _invalid -> {:error, "invalid product sort"}
-    end
-  end
-
-  defp normalize_product_sort(_sort), do: {:error, "invalid product sort"}
-
-  @spec normalize_numeric_filters(any()) :: {:ok, [map()]} | {:error, String.t()}
-  defp normalize_numeric_filters(filters) when is_list(filters) do
-    Enum.reduce_while(filters, {:ok, []}, fn filter, {:ok, acc} ->
-      with true <- is_map(filter),
-           {:ok, attribute_id} <-
-             Input.decode_required_integer_id(
-               Input.fetch_value(filter, :attribute_id),
-               :attribute,
-               "attribute"
-             ),
-           {:ok, min} <- Input.normalize_decimal_value(Input.fetch_value(filter, :min)),
-           {:ok, max} <- Input.normalize_decimal_value(Input.fetch_value(filter, :max)) do
-        normalized_filter =
-          %{attribute_id: attribute_id}
-          |> Input.put_present(:min, min)
-          |> Input.put_present(:max, max)
-
-        {:cont, {:ok, [normalized_filter | acc]}}
-      else
-        false -> {:halt, {:error, "invalid numeric filter"}}
-        {:error, _} = error -> {:halt, error}
-      end
-    end)
-    |> reverse_ok_list()
-  end
-
-  defp normalize_numeric_filters(_filters), do: {:error, "invalid numeric filter"}
-
-  @spec normalize_boolean_filters(any()) :: {:ok, [map()]} | {:error, String.t()}
-  defp normalize_boolean_filters(filters) when is_list(filters) do
-    Enum.reduce_while(filters, {:ok, []}, fn filter, {:ok, acc} ->
-      with true <- is_map(filter),
-           {:ok, attribute_id} <-
-             Input.decode_required_integer_id(
-               Input.fetch_value(filter, :attribute_id),
-               :attribute,
-               "attribute"
-             ),
-           value when is_boolean(value) <- Input.fetch_value(filter, :value) do
-        {:cont, {:ok, [%{attribute_id: attribute_id, value: value} | acc]}}
-      else
-        false -> {:halt, {:error, "invalid boolean filter"}}
-        {:error, _} = error -> {:halt, error}
-        _ -> {:halt, {:error, "invalid boolean filter"}}
-      end
-    end)
-    |> reverse_ok_list()
-  end
-
-  defp normalize_boolean_filters(_filters), do: {:error, "invalid boolean filter"}
-
-  @spec normalize_enum_filters(any()) :: {:ok, [map()]} | {:error, String.t()}
-  defp normalize_enum_filters(filters) when is_list(filters) do
-    Enum.reduce_while(filters, {:ok, []}, fn filter, {:ok, acc} ->
-      with true <- is_map(filter),
-           {:ok, attribute_id} <-
-             Input.decode_required_integer_id(
-               Input.fetch_value(filter, :attribute_id),
-               :attribute,
-               "attribute"
-             ),
-           {:ok, enum_option_id} <-
-             Input.decode_required_integer_id(
-               Input.fetch_value(filter, :enum_option_id),
-               :enum_option,
-               "enum option"
-             ) do
-        {:cont, {:ok, [%{attribute_id: attribute_id, enum_option_id: enum_option_id} | acc]}}
-      else
-        false -> {:halt, {:error, "invalid enum filter"}}
-        {:error, _} = error -> {:halt, error}
-      end
-    end)
-    |> reverse_ok_list()
-  end
-
-  defp normalize_enum_filters(_filters), do: {:error, "invalid enum filter"}
-
-  defp validate_filter_semantics(numeric_filters, boolean_filters, enum_filters) do
-    attribute_types =
-      [numeric_filters, boolean_filters, enum_filters]
-      |> Enum.flat_map(&Enum.map(&1, fn filter -> filter.attribute_id end))
-      |> Specs.filterable_attribute_types()
-
-    enum_option_pairs =
-      Specs.filterable_enum_option_pairs(
-        Enum.map(enum_filters, & &1.attribute_id),
-        Enum.map(enum_filters, & &1.enum_option_id)
-      )
-
-    with :ok <- validate_numeric_filter_semantics(numeric_filters, attribute_types),
-         :ok <- validate_boolean_filter_semantics(boolean_filters, attribute_types),
-         :ok <- validate_enum_filter_semantics(enum_filters, enum_option_pairs) do
-      :ok
-    end
-  end
-
-  defp validate_numeric_filter_semantics(filters, attribute_types) do
-    Enum.reduce_while(filters, :ok, fn filter, :ok ->
-      cond do
-        Map.get(attribute_types, filter.attribute_id) != :numeric ->
-          {:halt, {:error, "invalid numeric filter"}}
-
-        numeric_min_greater_than_max?(filter) ->
-          {:halt, {:error, "invalid numeric filter"}}
-
-        true ->
-          {:cont, :ok}
-      end
-    end)
-  end
-
-  defp validate_boolean_filter_semantics(filters, attribute_types) do
-    Enum.reduce_while(filters, :ok, fn filter, :ok ->
-      if Map.get(attribute_types, filter.attribute_id) == :bool do
-        {:cont, :ok}
-      else
-        {:halt, {:error, "invalid boolean filter"}}
-      end
-    end)
-  end
-
-  defp validate_enum_filter_semantics(filters, enum_option_pairs) do
-    Enum.reduce_while(filters, :ok, fn filter, :ok ->
-      if MapSet.member?(enum_option_pairs, {filter.attribute_id, filter.enum_option_id}) do
-        {:cont, :ok}
-      else
-        {:halt, {:error, "invalid enum filter"}}
-      end
-    end)
-  end
-
-  defp numeric_min_greater_than_max?(%{min: min, max: max})
-       when not is_nil(min) and not is_nil(max) do
-    Decimal.compare(to_decimal(min), to_decimal(max)) == :gt
-  end
-
-  defp numeric_min_greater_than_max?(_filter), do: false
-
-  defp to_decimal(%Decimal{} = value), do: value
-  defp to_decimal(value) when is_integer(value), do: Decimal.new(value)
-  defp to_decimal(value) when is_float(value), do: Decimal.from_float(value)
-
   defp load_taxon_attributes(loader, taxon_id) when is_integer(taxon_id) do
     Dataloader.load(loader, Catalog, {:many, TaxonAttribute}, taxon_id: taxon_id)
   end
@@ -536,11 +240,6 @@ defmodule ProductCompareWeb.Resolvers.CatalogResolver do
   end
 
   defp loaded_taxon_attributes(_loader, _taxon_id), do: []
-
-  @spec reverse_ok_list({:ok, list()} | {:error, String.t()}) ::
-          {:ok, list()} | {:error, String.t()}
-  defp reverse_ok_list({:ok, items}), do: {:ok, Enum.reverse(items)}
-  defp reverse_ok_list({:error, _message} = error), do: error
 
   defp format_current_attributes(current_attributes, resolution, correction_counts) do
     base_unit_symbols_by_dimension =
