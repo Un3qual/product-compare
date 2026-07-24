91aefa13daee2799f4ce39480acb9969585512a5
Ryan <Un3qual@users.noreply.github.com>
2026-07-23 17:26:54 -0700

    refactor: isolate revenue projection

--- lib/product_compare/commerce_attribution/revenue.ex
+++ lib/product_compare/commerce_attribution/revenue.ex
@@ -3,27 +3,12 @@ defmodule ProductCompare.CommerceAttribution.Revenue do
 
   alias ProductCompare.CommerceAttribution.Revenue.Aggregation
   alias ProductCompare.CommerceAttribution.Revenue.Filters
+  alias ProductCompare.CommerceAttribution.Revenue.Projection
 
   @spec dashboard_revenue_summary(map() | keyword()) :: map()
   def dashboard_revenue_summary(opts \\ %{}) do
     filters = Filters.normalize(opts)
-
-    metrics =
-      filters
-      |> Aggregation.metrics()
-      |> project_metrics()
-      |> Map.put("clicks", Aggregation.click_count(filters))
-
-    {metrics, suppressed?} = maybe_suppress_metrics(metrics, filters.min_conversions)
-
-    %{
-      "filters" => Filters.for_dashboard(filters),
-      "metrics" => metrics,
-      "suppression" => %{
-        "suppressed" => suppressed?,
-        "threshold" => filters.min_conversions
-      }
-    }
+    Projection.summary(filters, Aggregation.metrics(filters), Aggregation.click_count(filters))
   end
 
   @spec merchant_revenue_summary(pos_integer(), map() | keyword()) :: map()
@@ -46,35 +31,4 @@ defmodule ProductCompare.CommerceAttribution.Revenue do
     |> Filters.put(:network, network)
     |> dashboard_revenue_summary()
   end
-
-  defp project_metrics(metrics) do
-    %{
-      "average_paid_price" => nullable_money_string(metrics.average_paid_price),
-      "commission_revenue" => money_string(metrics.commission_revenue),
-      "conversions" => metrics.conversions,
-      "currency" => metrics.currency,
-      "gross_order_value" => money_string(metrics.gross_order_value)
-    }
-  end
-
-  defp maybe_suppress_metrics(metrics, min_conversions) when min_conversions > 0 do
-    if metrics["conversions"] < min_conversions do
-      {Map.new(Map.keys(metrics), &{&1, nil}), true}
-    else
-      {metrics, false}
-    end
-  end
-
-  defp maybe_suppress_metrics(metrics, _min_conversions), do: {metrics, false}
-
-  defp money_string(nil), do: "0.00"
-
-  defp money_string(%Decimal{} = decimal) do
-    decimal
-    |> Decimal.round(2)
-    |> Decimal.to_string(:normal)
-  end
-
-  defp nullable_money_string(nil), do: nil
-  defp nullable_money_string(%Decimal{} = decimal), do: money_string(decimal)
 end
