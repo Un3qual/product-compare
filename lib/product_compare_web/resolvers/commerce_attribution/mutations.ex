53d43c0d5e74e0aea533db53941eb84dd1f8b647
Ryan <Un3qual@users.noreply.github.com>
2026-07-23 17:29:07 -0700

    refactor: isolate graphql commerce clicks

---
+++ lib/product_compare_web/resolvers/commerce_attribution/mutations.ex
@@ -0,0 +1,71 @@
+defmodule ProductCompareWeb.Resolvers.CommerceAttribution.Mutations do
+  @moduledoc false
+
+  alias ProductCompare.CommerceAttribution
+  alias ProductCompareWeb.GraphQL.Errors, as: GraphQLErrors
+  alias ProductCompareWeb.GraphQL.Input
+
+  @invalid_origin_message "cross-origin request rejected"
+
+  @spec track_commerce_click(any(), %{input: map()}, Absinthe.Resolution.t()) :: {:ok, map()}
+  def track_commerce_click(_parent, %{input: input}, resolution) do
+    with :ok <- require_trusted_request_origin(resolution),
+         {:ok, merchant_product_id} <- decode_merchant_product_id(input),
+         {:ok, tracked_click} <-
+           CommerceAttribution.track_outbound_click(%{
+             merchant_product_id: merchant_product_id,
+             source_surface: :web,
+             user_id: current_user_id(resolution)
+           }) do
+      {:ok, %{redirect_path: tracked_click.redirect_path, errors: []}}
+    else
+      {:error, :invalid_origin} ->
+        {:ok, commerce_click_error_payload("INVALID_ORIGIN", @invalid_origin_message)}
+
+      {:error, :invalid_id} ->
+        {:ok,
+         commerce_click_error_payload(
+           "INVALID_ID",
+           "invalid merchant product id",
+           :merchant_product_id
+         )}
+
+      {:error, :merchant_product_not_found} ->
+        {:ok,
+         commerce_click_error_payload(
+           "NOT_FOUND",
+           "merchant product not found",
+           :merchant_product_id
+         )}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {field, message} = GraphQLErrors.changeset_first_error(changeset)
+        {:ok, commerce_click_error_payload("INVALID_ARGUMENT", message, field)}
+    end
+  end
+
+  defp decode_merchant_product_id(input) when is_map(input) do
+    input
+    |> Input.fetch_value(:merchant_product_id)
+    |> Input.decode_required_integer_id(:merchant_product, "merchant product")
+    |> case do
+      {:ok, merchant_product_id} -> {:ok, merchant_product_id}
+      {:error, _message} -> {:error, :invalid_id}
+    end
+  end
+
+  defp decode_merchant_product_id(_input), do: {:error, :invalid_id}
+
+  defp current_user_id(%{context: %{current_user: %{id: id}}}) when is_integer(id), do: id
+  defp current_user_id(_resolution), do: nil
+
+  defp require_trusted_request_origin(%{context: %{trusted_request_origin?: true}}), do: :ok
+  defp require_trusted_request_origin(_resolution), do: {:error, :invalid_origin}
+
+  defp commerce_click_error_payload(code, message, field \\ nil) do
+    %{
+      redirect_path: nil,
+      errors: [GraphQLErrors.camelized_mutation_error(code, message, field)]
+    }
+  end
+end
--- lib/product_compare_web/resolvers/commerce_attribution_resolver.ex
+++ lib/product_compare_web/resolvers/commerce_attribution_resolver.ex
@@ -1,12 +1,9 @@
 defmodule ProductCompareWeb.Resolvers.CommerceAttributionResolver do
   @moduledoc false
 
-  alias ProductCompare.CommerceAttribution
   alias ProductCompareWeb.GraphQL.Errors, as: GraphQLErrors
-  alias ProductCompareWeb.GraphQL.Input
+  alias ProductCompareWeb.Resolvers.CommerceAttribution.Mutations
   alias ProductCompareWeb.Resolvers.CommerceAttribution.Reads
-
-  @invalid_origin_message "cross-origin request rejected"
 
   @spec revenue_summary(any(), map(), Absinthe.Resolution.t()) ::
           {:ok, map()}
@@ -16,64 +13,6 @@ defmodule ProductCompareWeb.Resolvers.CommerceAttributionResolver do
     do: Reads.revenue_summary(parent, args, resolution)
 
   @spec track_commerce_click(any(), %{input: map()}, Absinthe.Resolution.t()) :: {:ok, map()}
-  def track_commerce_click(_parent, %{input: input}, resolution) do
-    with :ok <- require_trusted_request_origin(resolution),
-         {:ok, merchant_product_id} <- decode_merchant_product_id(input),
-         {:ok, tracked_click} <-
-           CommerceAttribution.track_outbound_click(%{
-             merchant_product_id: merchant_product_id,
-             source_surface: :web,
-             user_id: current_user_id(resolution)
-           }) do
-      {:ok, %{redirect_path: tracked_click.redirect_path, errors: []}}
-    else
-      {:error, :invalid_origin} ->
-        {:ok, commerce_click_error_payload("INVALID_ORIGIN", @invalid_origin_message)}
-
-      {:error, :invalid_id} ->
-        {:ok,
-         commerce_click_error_payload(
-           "INVALID_ID",
-           "invalid merchant product id",
-           :merchant_product_id
-         )}
-
-      {:error, :merchant_product_not_found} ->
-        {:ok,
-         commerce_click_error_payload(
-           "NOT_FOUND",
-           "merchant product not found",
-           :merchant_product_id
-         )}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {field, message} = GraphQLErrors.changeset_first_error(changeset)
-        {:ok, commerce_click_error_payload("INVALID_ARGUMENT", message, field)}
-    end
-  end
-
-  defp decode_merchant_product_id(input) when is_map(input) do
-    input
-    |> Input.fetch_value(:merchant_product_id)
-    |> Input.decode_required_integer_id(:merchant_product, "merchant product")
-    |> case do
-      {:ok, merchant_product_id} -> {:ok, merchant_product_id}
-      {:error, _message} -> {:error, :invalid_id}
-    end
-  end
-
-  defp decode_merchant_product_id(_input), do: {:error, :invalid_id}
-
-  defp current_user_id(%{context: %{current_user: %{id: id}}}) when is_integer(id), do: id
-  defp current_user_id(_resolution), do: nil
-
-  defp require_trusted_request_origin(%{context: %{trusted_request_origin?: true}}), do: :ok
-  defp require_trusted_request_origin(_resolution), do: {:error, :invalid_origin}
-
-  defp commerce_click_error_payload(code, message, field \\ nil) do
-    %{
-      redirect_path: nil,
-      errors: [GraphQLErrors.camelized_mutation_error(code, message, field)]
-    }
-  end
+  def track_commerce_click(parent, args, resolution),
+    do: Mutations.track_commerce_click(parent, args, resolution)
 end
