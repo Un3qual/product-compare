cdc5391a2106d9162f5d082d0786b02978edf765
Ryan <Un3qual@users.noreply.github.com>
2026-07-23 17:49:36 -0700

    refactor: isolate specification reference reads

--- lib/product_compare/specs/reads.ex
+++ lib/product_compare/specs/reads.ex
@@ -1,24 +1,20 @@
 defmodule ProductCompare.Specs.Reads do
   @moduledoc false
 
-  import Ecto.Query
-
   alias ProductCompare.Specs.Reads.Artifacts
   alias ProductCompare.Specs.Reads.CurrentAttributes
-  alias ProductCompare.Repo
+  alias ProductCompare.Specs.Reads.ReferenceData
   alias ProductCompareSchemas.Specs.Attribute
   alias ProductCompareSchemas.Specs.EnumOption
   alias ProductCompareSchemas.Specs.ProductAttributeCurrent
   alias ProductCompareSchemas.Specs.SourceArtifact
   alias ProductCompareSchemas.Specs.TaxonAttribute
-  alias ProductCompareSchemas.Specs.Unit
 
   @max_bigint_id 9_223_372_036_854_775_807
   defguardp valid_id_guard(id) when is_integer(id) and id > 0 and id <= @max_bigint_id
 
   @spec get_source_artifact(term()) :: SourceArtifact.t() | nil
   def get_source_artifact(id) when valid_id_guard(id), do: Artifacts.get(id)
-
   def get_source_artifact(_id), do: nil
 
   @spec get_source_artifacts([term()]) :: %{optional(pos_integer()) => SourceArtifact.t() | nil}
@@ -44,170 +40,59 @@ defmodule ProductCompare.Specs.Reads do
       do: CurrentAttributes.with_metadata(current_attributes, taxon_id)
 
   @spec list_filterable_attributes([atom()]) :: [Attribute.t()]
-  def list_filterable_attributes(data_types) when is_list(data_types) do
-    Repo.all(
-      from attribute in Attribute,
-        where: attribute.is_filterable == true,
-        where: attribute.data_type in ^data_types,
-        order_by: [asc: attribute.display_name, asc: attribute.code, asc: attribute.id]
-    )
-  end
+  def list_filterable_attributes(data_types) when is_list(data_types),
+    do: ReferenceData.list_filterable_attributes(data_types)
 
   @spec filterable_attribute_types(term()) :: %{pos_integer() => atom()}
-  def filterable_attribute_types(attribute_ids) when is_list(attribute_ids) do
-    attribute_ids
-    |> normalize_ids()
-    |> case do
-      [] ->
-        %{}
-
-      ids ->
-        Repo.all(
-          from attribute in Attribute,
-            where: attribute.id in ^ids,
-            where: attribute.is_filterable == true,
-            select: {attribute.id, attribute.data_type}
-        )
-        |> Map.new()
-    end
-  end
+  def filterable_attribute_types(attribute_ids) when is_list(attribute_ids),
+    do: ReferenceData.filterable_attribute_types(attribute_ids)
 
   def filterable_attribute_types(_attribute_ids), do: %{}
 
   @spec get_filterable_attribute(term(), term()) :: Attribute.t() | nil
   def get_filterable_attribute(attribute_id, data_type)
-      when valid_id_guard(attribute_id) and is_atom(data_type) do
-    Repo.one(
-      from attribute in Attribute,
-        where: attribute.id == ^attribute_id,
-        where: attribute.data_type == ^data_type,
-        where: attribute.is_filterable == true
-    )
-  end
+      when valid_id_guard(attribute_id) and is_atom(data_type),
+      do: ReferenceData.get_filterable_attribute(attribute_id, data_type)
 
   def get_filterable_attribute(_attribute_id, _data_type), do: nil
 
   @spec filterable_enum_option_pairs(term(), term()) :: MapSet.t()
   def filterable_enum_option_pairs(attribute_ids, enum_option_ids)
-      when is_list(attribute_ids) and is_list(enum_option_ids) do
-    attribute_ids = normalize_ids(attribute_ids)
-    enum_option_ids = normalize_ids(enum_option_ids)
+      when is_list(attribute_ids) and is_list(enum_option_ids),
+      do: ReferenceData.filterable_enum_option_pairs(attribute_ids, enum_option_ids)
 
-    if attribute_ids == [] or enum_option_ids == [] do
-      map_set([])
-    else
-      Repo.all(
-        from attribute in Attribute,
-          join: enum_option in EnumOption,
-          on: enum_option.enum_set_id == attribute.enum_set_id,
-          where: attribute.id in ^attribute_ids,
-          where: attribute.data_type == :enum,
-          where: attribute.is_filterable == true,
-          where: enum_option.id in ^enum_option_ids,
-          select: {attribute.id, enum_option.id}
-      )
-      |> map_set()
-    end
-  end
-
-  def filterable_enum_option_pairs(_attribute_ids, _enum_option_ids), do: map_set([])
+  def filterable_enum_option_pairs(_attribute_ids, _enum_option_ids), do: MapSet.new()
 
   @spec enum_option_belongs_to_attribute?(term(), term()) :: boolean()
   def enum_option_belongs_to_attribute?(attribute_id, enum_option_id)
-      when valid_id_guard(attribute_id) and valid_id_guard(enum_option_id) do
-    Repo.exists?(
-      from attribute in Attribute,
-        join: enum_option in EnumOption,
-        on: enum_option.enum_set_id == attribute.enum_set_id,
-        where: attribute.id == ^attribute_id,
-        where: attribute.data_type == :enum,
-        where: attribute.is_filterable == true,
-        where: enum_option.id == ^enum_option_id
-    )
-  end
+      when valid_id_guard(attribute_id) and valid_id_guard(enum_option_id),
+      do: ReferenceData.enum_option_belongs_to_attribute?(attribute_id, enum_option_id)
 
   def enum_option_belongs_to_attribute?(_attribute_id, _enum_option_id), do: false
 
   @spec list_enum_options_for_set(term()) :: [EnumOption.t()]
-  def list_enum_options_for_set(enum_set_id) when valid_id_guard(enum_set_id) do
-    Repo.all(
-      from enum_option in EnumOption,
-        where: enum_option.enum_set_id == ^enum_set_id,
-        order_by: [asc: enum_option.sort_order, asc: enum_option.label, asc: enum_option.id]
-    )
-  end
+  def list_enum_options_for_set(enum_set_id) when valid_id_guard(enum_set_id),
+    do: ReferenceData.list_enum_options_for_set(enum_set_id)
 
   def list_enum_options_for_set(_enum_set_id), do: []
 
   @spec list_enum_options_for_sets(term()) :: %{pos_integer() => [EnumOption.t()]}
-  def list_enum_options_for_sets(enum_set_ids) when is_list(enum_set_ids) do
-    enum_set_ids
-    |> normalize_ids()
-    |> case do
-      [] ->
-        %{}
-
-      ids ->
-        Repo.all(
-          from enum_option in EnumOption,
-            where: enum_option.enum_set_id in ^ids,
-            order_by: [
-              asc: enum_option.enum_set_id,
-              asc: enum_option.sort_order,
-              asc: enum_option.label,
-              asc: enum_option.id
-            ]
-        )
-        |> Enum.group_by(& &1.enum_set_id)
-    end
-  end
+  def list_enum_options_for_sets(enum_set_ids) when is_list(enum_set_ids),
+    do: ReferenceData.list_enum_options_for_sets(enum_set_ids)
 
   def list_enum_options_for_sets(_enum_set_ids), do: %{}
 
   @spec unit_symbol_for_dimension(term()) :: String.t() | nil
   def unit_symbol_for_dimension(nil), do: nil
 
-  def unit_symbol_for_dimension(dimension_id) when valid_id_guard(dimension_id) do
-    dimension_id
-    |> List.wrap()
-    |> unit_symbols_for_dimensions()
-    |> Map.get(dimension_id)
-  end
+  def unit_symbol_for_dimension(dimension_id) when valid_id_guard(dimension_id),
+    do: ReferenceData.unit_symbol_for_dimension(dimension_id)
 
   def unit_symbol_for_dimension(_dimension_id), do: nil
 
   @spec unit_symbols_for_dimensions(term()) :: %{pos_integer() => String.t()}
-  def unit_symbols_for_dimensions(dimension_ids) when is_list(dimension_ids) do
-    dimension_ids
-    |> normalize_ids()
-    |> case do
-      [] ->
-        %{}
-
-      ids ->
-        Repo.all(
-          from unit in Unit,
-            where: unit.dimension_id in ^ids,
-            order_by: [
-              asc: unit.dimension_id,
-              asc:
-                fragment(
-                  "CASE WHEN ? = 1 AND ? = 0 THEN 0 ELSE 1 END",
-                  unit.multiplier_to_base,
-                  unit.offset_to_base
-                ),
-              asc: unit.id
-            ],
-            select: {
-              unit.dimension_id,
-              fragment("COALESCE(NULLIF(?, ''), NULLIF(?, ''))", unit.symbol, unit.code)
-            }
-        )
-        |> Enum.reduce(%{}, fn {dimension_id, symbol}, acc ->
-          Map.put_new(acc, dimension_id, symbol)
-        end)
-    end
-  end
+  def unit_symbols_for_dimensions(dimension_ids) when is_list(dimension_ids),
+    do: ReferenceData.unit_symbols_for_dimensions(dimension_ids)
 
   def unit_symbols_for_dimensions(_dimension_ids), do: %{}
 
@@ -222,16 +107,4 @@ defmodule ProductCompare.Specs.Reads do
           current_attributes,
           taxon_attributes
         )
-
-  defp normalize_ids(ids) do
-    ids
-    |> Enum.filter(&valid_id?/1)
-    |> Enum.uniq()
-  end
-
-  defp valid_id?(id), do: is_integer(id) and id > 0 and id <= @max_bigint_id
-
-  defp map_set(values) do
-    Enum.reduce(values, MapSet.new(), &MapSet.put(&2, &1))
-  end
 end
