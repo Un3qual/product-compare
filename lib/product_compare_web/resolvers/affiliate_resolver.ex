9e893f201266bab819d4439796b664f403fae0fe
Ryan <Un3qual@users.noreply.github.com>
2026-07-23 18:06:35 -0700

    refactor: isolate graphql affiliate mutations

--- lib/product_compare_web/resolvers/affiliate_resolver.ex
+++ lib/product_compare_web/resolvers/affiliate_resolver.ex
@@ -1,81 +1,29 @@
 defmodule ProductCompareWeb.Resolvers.AffiliateResolver do
   @moduledoc false
 
-  alias ProductCompare.Affiliate
+  alias ProductCompareWeb.Resolvers.Affiliate.Mutations
   alias ProductCompareWeb.Resolvers.Affiliate.Reads
-  alias ProductCompareWeb.GraphQL.Authorization
   alias ProductCompareWeb.GraphQL.Errors, as: GraphQLErrors
-  alias ProductCompareWeb.GraphQL.Input
 
   @spec upsert_affiliate_network(any(), map(), Absinthe.Resolution.t()) ::
           {:ok, map()}
-  def upsert_affiliate_network(_parent, %{input: input}, resolution) do
-    with {:ok, _user} <- Authorization.require_operator(resolution) do
-      attrs = Input.take_present(input, [:name])
-
-      case Affiliate.upsert_network(attrs) do
-        {:ok, network} ->
-          {:ok, %{network: network, errors: []}}
-
-        {:error, changeset} ->
-          {field, message} = GraphQLErrors.changeset_first_error(changeset)
-          {:ok, mutation_error_payload(:network, "INVALID_ARGUMENT", message, field)}
-      end
-    else
-      {:error, reason} ->
-        {:ok,
-         mutation_error_payload(:network, GraphQLErrors.authorization_mutation_error(reason))}
-    end
-  end
-
-  def upsert_affiliate_network(_parent, _args, resolution),
-    do: denied_mutation(:network, resolution)
+  def upsert_affiliate_network(parent, args, resolution),
+    do: Mutations.upsert_affiliate_network(parent, args, resolution)
 
   @spec upsert_affiliate_program(any(), map(), Absinthe.Resolution.t()) ::
           {:ok, map()}
-  def upsert_affiliate_program(_parent, %{input: input}, resolution) do
-    operator_affiliate_mutation(
-      resolution,
-      :program,
-      input,
-      [:affiliate_network_id, :merchant_id],
-      program_attrs(),
-      &Affiliate.upsert_program/1
-    )
-  end
-
-  def upsert_affiliate_program(_parent, _args, resolution),
-    do: denied_mutation(:program, resolution)
+  def upsert_affiliate_program(parent, args, resolution),
+    do: Mutations.upsert_affiliate_program(parent, args, resolution)
 
   @spec upsert_affiliate_link(any(), map(), Absinthe.Resolution.t()) ::
           {:ok, map()}
-  def upsert_affiliate_link(_parent, %{input: input}, resolution) do
-    operator_affiliate_mutation(
-      resolution,
-      :link,
-      input,
-      [:merchant_product_id, :affiliate_network_id],
-      link_attrs(),
-      &Affiliate.upsert_link/1
-    )
-  end
-
-  def upsert_affiliate_link(_parent, _args, resolution), do: denied_mutation(:link, resolution)
+  def upsert_affiliate_link(parent, args, resolution),
+    do: Mutations.upsert_affiliate_link(parent, args, resolution)
 
   @spec create_coupon(any(), map(), Absinthe.Resolution.t()) ::
           {:ok, map()}
-  def create_coupon(_parent, %{input: input}, resolution) do
-    operator_affiliate_mutation(
-      resolution,
-      :coupon,
-      input,
-      [:merchant_id, :affiliate_network_id, :artifact_id],
-      coupon_attrs(),
-      &Affiliate.create_coupon/1
-    )
-  end
-
-  def create_coupon(_parent, _args, resolution), do: denied_mutation(:coupon, resolution)
+  def create_coupon(parent, args, resolution),
+    do: Mutations.create_coupon(parent, args, resolution)
 
   @spec active_coupons(any(), map(), Absinthe.Resolution.t()) ::
           {:ok, map()}
@@ -87,126 +35,4 @@ defmodule ProductCompareWeb.Resolvers.AffiliateResolver do
           {:ok, map()} | {:error, String.t()}
   def merchant_product_active_coupons(parent, args, resolution),
     do: Reads.merchant_product_active_coupons(parent, args, resolution)
-
-  defp normalize_attrs(attrs, id_fields, attr_fields) do
-    with {:ok, attrs} <- normalize_ids(attrs, id_fields) do
-      {:ok, Input.take(attrs, attr_fields)}
-    end
-  end
-
-  defp affiliate_mutation(entity_field, input, id_fields, attr_fields, save_fun) do
-    with {:ok, attrs} <- normalize_attrs(input, id_fields, attr_fields),
-         {:ok, entity} <- save_fun.(attrs) do
-      {:ok, %{entity_field => entity, errors: []}}
-    else
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {field, message} = GraphQLErrors.changeset_first_error(changeset)
-        {:ok, mutation_error_payload(entity_field, "INVALID_ARGUMENT", message, field)}
-
-      {:error, {:invalid_id, field}} ->
-        {:ok,
-         mutation_error_payload(entity_field, "INVALID_ID", invalid_id_message(field), field)}
-
-      {:error, reason} when is_binary(reason) ->
-        {:ok, mutation_error_payload(entity_field, "INVALID_ARGUMENT", reason)}
-    end
-  end
-
-  defp operator_affiliate_mutation(
-         resolution,
-         entity_field,
-         input,
-         id_fields,
-         attr_fields,
-         save_fun
-       ) do
-    with {:ok, _user} <- Authorization.require_operator(resolution) do
-      affiliate_mutation(entity_field, input, id_fields, attr_fields, save_fun)
-    else
-      {:error, reason} ->
-        {:ok,
-         mutation_error_payload(entity_field, GraphQLErrors.authorization_mutation_error(reason))}
-    end
-  end
-
-  defp denied_mutation(entity_field, resolution) do
-    {:error, reason} = Authorization.require_operator(resolution)
-
-    {:ok,
-     mutation_error_payload(entity_field, GraphQLErrors.authorization_mutation_error(reason))}
-  end
-
-  defp normalize_ids(attrs, id_fields) when is_map(attrs) do
-    Enum.reduce_while(id_fields, {:ok, attrs}, fn field, {:ok, acc} ->
-      case Input.decode_optional_integer_id_field(
-             acc,
-             field,
-             field_type(field),
-             id_field_name(field)
-           ) do
-        {:ok, attrs} ->
-          {:cont, {:ok, attrs}}
-
-        {:error, _message} ->
-          {:halt, {:error, {:invalid_id, field}}}
-      end
-    end)
-  end
-
-  defp field_type(:affiliate_network_id), do: :affiliate_network
-  defp field_type(:merchant_id), do: :merchant
-  defp field_type(:merchant_product_id), do: :merchant_product
-  defp field_type(:artifact_id), do: :source_artifact
-  defp field_type(_field), do: nil
-
-  defp program_attrs, do: [:affiliate_network_id, :merchant_id, :program_code, :status]
-
-  defp link_attrs,
-    do: [
-      :merchant_product_id,
-      :affiliate_network_id,
-      :original_url,
-      :affiliate_url,
-      :last_verified_at
-    ]
-
-  defp coupon_attrs,
-    do: [
-      :merchant_id,
-      :affiliate_network_id,
-      :artifact_id,
-      :code,
-      :description,
-      :discount_type,
-      :discount_value,
-      :currency,
-      :valid_from,
-      :valid_to,
-      :terms
-    ]
-
-  defp id_field_name(field) do
-    field
-    |> Atom.to_string()
-    |> String.replace_suffix("_id", "")
-    |> String.replace("_", " ")
-  end
-
-  defp mutation_error_payload(entity_field, code, message, field \\ nil) do
-    %{
-      entity_field => nil,
-      errors: [GraphQLErrors.camelized_mutation_error(code, message, field)]
-    }
-  end
-
-  defp mutation_error_payload(entity_field, error) when is_map(error) do
-    %{
-      entity_field => nil,
-      errors: [error]
-    }
-  end
-
-  defp invalid_id_message(field) do
-    "invalid #{id_field_name(field)} id"
-  end
 end
