55d4c5f1ebc3b12eb1cc7b6a28bddf48d1acfd7d
Ryan <Un3qual@users.noreply.github.com>
2026-07-23 18:20:28 -0700

    refactor: isolate graphql price evidence

--- lib/product_compare_web/resolvers/pricing_resolver.ex
+++ lib/product_compare_web/resolvers/pricing_resolver.ex
@@ -1,9 +1,7 @@
 defmodule ProductCompareWeb.Resolvers.PricingResolver do
   @moduledoc false
 
-  import Absinthe.Resolution.Helpers, only: [on_load: 2]
-
-  alias ProductCompare.Pricing
+  alias ProductCompareWeb.Resolvers.Pricing.Evidence
   alias ProductCompareWeb.Resolvers.Pricing.Merchants
   alias ProductCompareWeb.Resolvers.Pricing.Offers
   alias ProductCompareSchemas.Specs.SourceArtifact
@@ -37,18 +35,8 @@ defmodule ProductCompareWeb.Resolvers.PricingResolver do
 
   @spec source_artifact(map(), map(), Absinthe.Resolution.t()) ::
           {:ok, SourceArtifact.t() | nil} | Absinthe.Resolution.Helpers.dataloader_tuple()
-  def source_artifact(%{artifact_id: nil}, _args, _resolution), do: {:ok, nil}
-
-  def source_artifact(%{artifact_id: artifact_id}, _args, %{context: %{loader: loader}})
-      when is_integer(artifact_id) do
-    loader
-    |> Dataloader.load(Pricing, {:one, SourceArtifact}, id: artifact_id)
-    |> on_load(fn loader ->
-      {:ok, Dataloader.get(loader, Pricing, {:one, SourceArtifact}, id: artifact_id)}
-    end)
-  end
-
-  def source_artifact(_price_point, _args, _resolution), do: {:ok, nil}
+  def source_artifact(parent, args, resolution),
+    do: Evidence.source_artifact(parent, args, resolution)
 
   @spec product_offer_truth(map(), map(), Absinthe.Resolution.t()) ::
           {:ok, map()} | Absinthe.Resolution.Helpers.dataloader_tuple()
